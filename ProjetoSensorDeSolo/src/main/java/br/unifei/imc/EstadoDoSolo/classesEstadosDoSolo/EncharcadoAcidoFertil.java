0f94c76fdebd7fc51e45333fb825fe0e06322604
RyanEXT <ryanaugustoo71@hotmail.com>
2022-11-28 20:26:40 -0300

    Add corrigeSolo em EncharcadoAcidoFertil
    Adicionando função em EncharcadoAcidoFertil

--- ProjetoSensorDeSolo/src/main/java/br/unifei/imc/EstadoDoSolo/classesEstadosDoSolo/EncharcadoAcidoFertil.java
+++ ProjetoSensorDeSolo/src/main/java/br/unifei/imc/EstadoDoSolo/classesEstadosDoSolo/EncharcadoAcidoFertil.java
@@ -5,19 +5,31 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import br.unifei.imc.plantacaoTipo.classes.*;
+import br.unifei.imc.plantacaoTipo.Plantacao;
 
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 public class EncharcadoAcidoFertil implements ProblemaSolo {
-    private int umidadeIni, phIni, fertIni;
+    private int umidade, ph, fert;
         @Override
-    public void corrigeSolo() {
-            int umidadeIdeal = 60 , phIdeal = 7, fertIdeal = 60;
+    public void corrigeSolo(Plantacao plantacao) {
+            int umidadeIdeal = plantacao.getUmidade(),
+            int phIdeal = plantacao.getPh(),
+            int fertIdeal = plantacao.getFertilizante();
+            
             System.out.println("Umidade Ideal >" + umidadeIdeal + " - Umidade atual >" + umidadeIni);
             System.out.println("PH Ideal >" + phIdeal + " - PH atual >" + phIni);
             System.out.println(" Fertilizante Ideal >" + fertIdeal + " - Fertilizante atual >" + fertIni);
-            System.out.println("Efetuando Correção do Solo");
+            System.out.println("Efetuando correções do solo:");
+
+            if(umidadeIdeal < umidade){
+                System.out.println("Drenando o solo...");
+            }
+            if(phIdeal > ph){
+                System.out.println("Adicionando calcário...");
+            }
     }
 }
