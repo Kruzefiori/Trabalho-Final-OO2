2ccdc3d1ffbbc3bc800d8c79ddbfde40ae0a6cdd
RyanEXT <ryanaugustoo71@hotmail.com>
2022-11-28 20:47:01 -0300

    Corrigindo duplicata EncharcadoNeutroFertil
    Corrigindo duplicata EncharcadoNeutroFertil

--- ProjetoSensorDeSolo/src/main/java/br/unifei/imc/EstadoDoSolo/classesEstadosDoSolo/EncharcadoNeutroFertil.java
+++ ProjetoSensorDeSolo/src/main/java/br/unifei/imc/EstadoDoSolo/classesEstadosDoSolo/EncharcadoNeutroFertil.java
@@ -16,22 +16,21 @@ import br.unifei.imc.plantacaoTipo.Plantacao;
 public class EncharcadoNeutroFertil implements ProblemaSolo {
     private int umidade, ph, fert;
     @Override
-    public void corrigeSolo() {
-        public void corrigeSolo(Plantacao plantacao) {
-                int umidadeIdeal = plantacao.getUmidade(),
-                int phIdeal = plantacao.getPh(),
-                int fertIdeal = plantacao.getFertilizante();
-                
-                System.out.println("Umidade Ideal >" + umidadeIdeal + " - Umidade atual >" + umidade);
-                System.out.println("PH Ideal >" + phIdeal + " - PH atual >" + ph);
-                System.out.println(" Fertilizante Ideal >" + fertIdeal + " - Fertilizante atual >" + fert);
-                System.out.println("Efetuando correções do solo:");
-    
-                if(umidadeIdeal < umidade){
-                    System.out.println("Drenando o solo...");
-                }
-              
-        }
+    public void corrigeSolo(Plantacao plantacao) {
+            int umidadeIdeal = plantacao.getUmidade(),
+            int phIdeal = plantacao.getPh(),
+            int fertIdeal = plantacao.getFertilizante();
+            
+            System.out.println("Umidade Ideal >" + umidadeIdeal + " - Umidade atual >" + umidade);
+            System.out.println("PH Ideal >" + phIdeal + " - PH atual >" + ph);
+            System.out.println(" Fertilizante Ideal >" + fertIdeal + " - Fertilizante atual >" + fert);
+            System.out.println("Efetuando correções do solo:");
 
+            if(umidadeIdeal < umidade){
+                System.out.println("Drenando o solo...");
+            }
+            
     }
+
 }
+
