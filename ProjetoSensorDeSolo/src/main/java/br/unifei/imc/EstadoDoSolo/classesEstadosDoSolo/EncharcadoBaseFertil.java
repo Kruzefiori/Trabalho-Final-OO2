2386e90ca4d070d0d6fcb4eaaa18e90f47819af9
RyanEXT <ryanaugustoo71@hotmail.com>
2022-11-28 20:44:50 -0300

    Corrigindo duplicata
    Corrigindo duplicata em função ProblemaSolo

--- ProjetoSensorDeSolo/src/main/java/br/unifei/imc/EstadoDoSolo/classesEstadosDoSolo/EncharcadoBaseFertil.java
+++ ProjetoSensorDeSolo/src/main/java/br/unifei/imc/EstadoDoSolo/classesEstadosDoSolo/EncharcadoBaseFertil.java
@@ -17,25 +17,24 @@ public class EncharcadoBaseFertil implements ProblemaSolo {
     private int umidade, ph, fert;
 
     @Override
-    public void corrigeSolo() {
-        public void corrigeSolo(Plantacao plantacao) {
-                int umidadeIdeal = plantacao.getUmidade(),
-                int phIdeal = plantacao.getPh(),
-                int fertIdeal = plantacao.getFertilizante();
-                
-                System.out.println("Umidade Ideal >" + umidadeIdeal + " - Umidade atual >" + umidade);
-                System.out.println("PH Ideal >" + phIdeal + " - PH atual >" + ph);
-                System.out.println(" Fertilizante Ideal >" + fertIdeal + " - Fertilizante atual >" + fert);
-                System.out.println("Efetuando correções do solo:");
-    
-                if(umidadeIdeal < umidade){
-                    System.out.println("Drenando o solo...");
-                }
-                if(phIdeal < ph){
-                    System.out.println("Adicionando húmus...");
-                }
-               
-        }
+    public void corrigeSolo(Plantacao plantacao) {
+            int umidadeIdeal = plantacao.getUmidade(),
+            int phIdeal = plantacao.getPh(),
+            int fertIdeal = plantacao.getFertilizante();
+            
+            System.out.println("Umidade Ideal >" + umidadeIdeal + " - Umidade atual >" + umidade);
+            System.out.println("PH Ideal >" + phIdeal + " - PH atual >" + ph);
+            System.out.println(" Fertilizante Ideal >" + fertIdeal + " - Fertilizante atual >" + fert);
+            System.out.println("Efetuando correções do solo:");
+
+            if(umidadeIdeal < umidade){
+                System.out.println("Drenando o solo...");
+            }
+            if(phIdeal < ph){
+                System.out.println("Adicionando húmus...");
+            }
+            
+    }
 
     }
 }
