2f2fef59a826ef8a276074ddd2d941dd9ffd2619
RyanEXT <83303260+RyanForward@users.noreply.github.com>
2022-11-30 14:26:20 -0300

    Concertando erros de digitação
    Trocando , por ;

--- ProjetoSensorDeSolo/src/main/java/br/unifei/imc/EstadoDoSolo/classesEstadosDoSolo/UmidoNeutroFertil.java
+++ ProjetoSensorDeSolo/src/main/java/br/unifei/imc/EstadoDoSolo/classesEstadosDoSolo/UmidoNeutroFertil.java
@@ -11,9 +11,28 @@ import lombok.Setter;
 @AllArgsConstructor
 @NoArgsConstructor
 public class UmidoNeutroFertil implements ProblemaSolo {
+<<<<<<< Updated upstream
     private int umidadeIni, phIni, fertIni;
     @Override
     public void corrigeSolo() {
 
     }
+=======
+    private int umidade, ph, fert;
+       @Override
+       public void corrigeSolo(Plantacao plantacao) {
+            int umidadeIdeal = plantacao.getUmidade();
+            int phIdeal = plantacao.getPh();
+            int fertIdeal = plantacao.getFertilizante();
+            
+            System.out.println("Umidade Ideal >" + umidadeIdeal + " - Umidade atual >" + umidade);
+            System.out.println("PH Ideal >" + phIdeal + " - PH atual >" + ph);
+            System.out.println(" Fertilizante Ideal >" + fertIdeal + " - Fertilizante atual >" + fert);
+            System.out.println("Efetuando correções do solo:");
+                
+            if(umidadeIdeal > umidade){
+                System.out.println("Umidificando o solo...");
+            }
+       }
+>>>>>>> Stashed changes
 }
--- ProjetoSensorDeSolo/src/main/java/br/unifei/imc/EstadoDoSolo/classesEstadosDoSolo/UmidoNeutroInfertil.java
+++ ProjetoSensorDeSolo/src/main/java/br/unifei/imc/EstadoDoSolo/classesEstadosDoSolo/UmidoNeutroInfertil.java
@@ -11,9 +11,31 @@ import lombok.Setter;
 @AllArgsConstructor
 @NoArgsConstructor
 public class UmidoNeutroInfertil implements ProblemaSolo {
+<<<<<<< Updated upstream
     private int umidadeIni, phIni, fertIni;
     @Override
     public void corrigeSolo() {
 
     }
+=======
+    private int umidade, ph, fert;
+       @Override
+       public void corrigeSolo(Plantacao plantacao) {
+            int umidadeIdeal = plantacao.getUmidade();
+            int phIdeal = plantacao.getPh();
+            int fertIdeal = plantacao.getFertilizante();
+            
+            System.out.println("Umidade Ideal >" + umidadeIdeal + " - Umidade atual >" + umidade);
+            System.out.println("PH Ideal >" + phIdeal + " - PH atual >" + ph);
+            System.out.println(" Fertilizante Ideal >" + fertIdeal + " - Fertilizante atual >" + fert);
+            System.out.println("Efetuando correções do solo:");
+                
+            if(umidadeIdeal > umidade){
+                System.out.println("Umidificando o solo...");
+            } 
+            if(fert < fertIdeal){
+                System.out.println("Adicionando fertilizante...");
+            }
+       }
+>>>>>>> Stashed changes
 }
