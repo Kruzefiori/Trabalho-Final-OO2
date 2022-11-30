d2f9bede847caef16a5933ed64afc2ecd354a2de
RyanEXT <83303260+RyanForward@users.noreply.github.com>
2022-11-30 14:55:54 -0300

    Corrigindo imports

--- ProjetoSensorDeSolo/src/main/java/br/unifei/imc/EstadoDoSolo/classesEstadosDoSolo/EncharcadoAcidoFertil.java
+++ ProjetoSensorDeSolo/src/main/java/br/unifei/imc/EstadoDoSolo/classesEstadosDoSolo/EncharcadoAcidoFertil.java
@@ -14,15 +14,15 @@ import br.unifei.imc.plantacaoTipo.Plantacao;
 @NoArgsConstructor
 public class EncharcadoAcidoFertil implements ProblemaSolo {
     private int umidade, ph, fert;
-        @Override
+    @Override
     public void corrigeSolo(Plantacao plantacao) {
             int umidadeIdeal = plantacao.getUmidade();
             int phIdeal = plantacao.getPh();
             int fertIdeal = plantacao.getFertilizante();
-            
-            System.out.println("Umidade Ideal >" + umidadeIdeal + " - Umidade atual >" + umidadeIni);
-            System.out.println("PH Ideal >" + phIdeal + " - PH atual >" + phIni);
-            System.out.println(" Fertilizante Ideal >" + fertIdeal + " - Fertilizante atual >" + fertIni);
+
+            System.out.println("Umidade Ideal >" + umidadeIdeal + " - Umidade atual >" + umidade);
+            System.out.println("PH Ideal >" + phIdeal + " - PH atual >" + ph);
+            System.out.println(" Fertilizante Ideal >" + fertIdeal + " - Fertilizante atual >" + fert);
             System.out.println("Efetuando correções do solo:");
 
             if(umidadeIdeal < umidade){
@@ -32,4 +32,5 @@ public class EncharcadoAcidoFertil implements ProblemaSolo {
                 System.out.println("Adicionando calcário...");
             }
     }
+
 }
