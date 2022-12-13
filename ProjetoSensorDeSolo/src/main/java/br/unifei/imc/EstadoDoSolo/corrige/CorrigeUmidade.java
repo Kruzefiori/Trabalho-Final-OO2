68aca56bbdebe6543ef4888c7c4583f23993a652
Vitória Thaís <vitoriathdasilva@gmail.com>
2022-12-13 14:39:42 -0300

    Adicionando classe CorrigeUmidade

---
+++ ProjetoSensorDeSolo/src/main/java/br/unifei/imc/EstadoDoSolo/corrige/CorrigeUmidade.java
@@ -0,0 +1,20 @@
+package br.unifei.imc.EstadoDoSolo.corrige;
+
+import br.unifei.imc.plantacaoTipo.Plantacao;
+
+public class CorrigeUmidade implements Corrige{
+    @Override
+    public void corrige_solo(Plantacao plantacao, int umidade, int ph, int fert) {
+        int umidadeIdeal = plantacao.getUmidade();
+
+        System.out.println("Umidade Ideal >" + umidadeIdeal + " - Umidade atual >" + umidade);
+     
+        if(umidadeIdeal < umidade){
+            System.out.println("Drenando o solo...");
+        }
+        if(umidadeIdeal > umidade){
+            System.out.println("Umidificando o solo...");
+        }
+
+    }
+}
