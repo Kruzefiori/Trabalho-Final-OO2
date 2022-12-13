02c31a416d69ad9d61dec235f26078d9a7a6994b
Vitória Thaís <vitoriathdasilva@gmail.com>
2022-12-13 15:22:06 -0300

    Corrigindo SecoAcidoInfertil

--- ProjetoSensorDeSolo/src/main/java/br/unifei/imc/EstadoDoSolo/classesEstadosDoSolo/SecoAcidoInfertil.java
+++ ProjetoSensorDeSolo/src/main/java/br/unifei/imc/EstadoDoSolo/classesEstadosDoSolo/SecoAcidoInfertil.java
@@ -1,6 +1,10 @@
 package br.unifei.imc.EstadoDoSolo.classesEstadosDoSolo;
 
 import br.unifei.imc.EstadoDoSolo.ProblemaSolo;
+import br.unifei.imc.EstadoDoSolo.corrige.Corrige;
+import br.unifei.imc.EstadoDoSolo.corrige.CorrigeFertilizante;
+import br.unifei.imc.EstadoDoSolo.corrige.CorrigePh;
+import br.unifei.imc.EstadoDoSolo.corrige.CorrigeUmidade;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -14,26 +18,23 @@ import br.unifei.imc.plantacaoTipo.Plantacao;
 @NoArgsConstructor
 public class SecoAcidoInfertil implements ProblemaSolo {
     private int umidade, ph, fert;
-       @Override
-       public void corrigeSolo(Plantacao plantacao) {
-           int umidadeIdeal = plantacao.getUmidade(),
-           int phIdeal = plantacao.getPh(),
-           int fertIdeal = plantacao.getFertilizante();
-           
-           System.out.println("Umidade Ideal >" + umidadeIdeal + " - Umidade atual >" + umidade);
-           System.out.println("PH Ideal >" + phIdeal + " - PH atual >" + ph);
-           System.out.println(" Fertilizante Ideal >" + fertIdeal + " - Fertilizante atual >" + fert);
-           System.out.println("Efetuando correções do solo:");
-   
-           if(umidadeIdeal > umidade){
-               System.out.println("Umidificando o solo...");
-           }
-           if(phIdeal > ph){
-               System.out.println("Adicionando calcário...");
-           }
-            if(fert< fertIdeal){
-            System.out.println("Adicionando fertilizante...");
-            }       
-       }
+    private Corrige corrigeumidade;
+    private Corrige corrigeph;
+
+    private Corrige corrigeFert;
+    public SecoAcidoInfertil(int umidade, int ph, int fert) {
+        this.umidade = umidade;
+        this.ph = ph;
+        this.fert = fert;
+        this.corrigeumidade = new CorrigeUmidade();
+        this.corrigeph = new CorrigePh() ;
+        this.corrigeFert = new CorrigeFertilizante();
+    }
+    @Override
+    public void corrigeSolo(Plantacao plantacao) {
+        this.corrigeumidade.corrige_solo(plantacao, this.umidade,this.ph, this.fert);
+        this.corrigeph.corrige_solo(plantacao, this.umidade,this.ph, this.fert);
+        this.corrigeFert.corrige_solo(plantacao, this.umidade,this.ph, this.fert);
+    }
 }
 
